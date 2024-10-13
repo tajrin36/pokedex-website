b3d1f4b172372b40ea74aa14b0dd79ad9501dee9
husna_tajrin <husnajahantajrin@gmail.com>
2024-10-13 19:11:58 +0600

    completed

--- detail.js
+++ detail.js
@@ -1,1 +1,60 @@
+let currentPokemonId = null;
+document.addEventListener("DOMContentLoaded", () => {
+  const MAX_POKEMON = 650;
+  const pokemonId = new URLSearchParams(window.location.search).get("id");
+  const id = parseInt(pokemonId, 10);
 
+  if (id < 1 || id > MAX_POKEMON) {
+    return (window.location.href = "./index.html");
+  }
+
+  currentPokemonId = id;
+  loadPokemon(id);
+});
+
+async function loadPokemon(id) {
+  try {
+    const [pokemon, pokemonSpecies] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) =>
+        response.json()
+      ),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then(
+        (response) => response.json()
+      ),
+    ]);
+
+    const abilitiesWrapper = document.querySelector(
+      ".pokemon-detail-wrap .pokemon-detail .move"
+    );
+    abilitiesWrapper.innerHTML = "";
+
+    if (currentPokemonId === id) {
+      displayPokemonDetail(pokemon);
+      const flavorText = getEnglishFlavorText(pokemonSpecies);
+      document.querySelector(".body3-fonts .pokemon-description").textContent =
+        flavorText;
+
+      const [leftArrow, rightArrow] = ["#leftArrow", "#rightArrow"].map((set) =>
+        document.querySelector(set)
+      );
+      leftArrow.removeEventListener("click", navigatePokemon);
+      rightArrow.removeEventListener("click", navigatePokemon);
+
+      if (id !== 1) {
+        leftArrow.addEventListener("click", () => {
+          navigatePokemon(id - 1);
+        });
+      }
+      if (id !== 650) {
+        rightArrow.addEventListener("click", () => {
+          navigatePokemon(id + 1);
+        });
+      }
+      window.history.pushState({},"",`./detail.html?id=${id}`)
+    }
+    return true;
+  } catch (error) {
+    console.error("An error occured fetching pokemon data", error);
+    return false;
+  }
+}
--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-const MAX_POKEMON = 1000;
+const MAX_POKEMON = 650;
 const listWrapper = document.querySelector(".list-warpper");
 const searchInput = document.querySelector("#search-input");
 const numberFilter = document.querySelector("#number");
